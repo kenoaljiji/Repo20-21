b32c6a2e97226221bcd3140896f523aebaef4ac0
kenoaljiji <studioaljiji@gmail.com>
2021-10-03 22:55:58 +0200

    finishing javascript, html , css  and adding some extra features

---
+++ ex01/src/index.js
@@ -0,0 +1,77 @@
+const button = document.querySelectorAll(".button");
+const button2 = document.querySelectorAll(".button2");
+const textArea = document.getElementById("display");
+const capsLock = document.getElementById("capsLock");
+const shift = document.getElementById("shift");
+
+console.log(button2);
+let text = "";
+
+//Change a character on specific breakpoint
+
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+if (viewportWidth < 700) {
+  button2[0].innerHTML = "&larr;";
+  button2[1].innerHTML = "&#8682;";
+} else {
+}
+
+window.addEventListener(
+  "resize",
+  function () {
+    viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth < 700) {
+      button2[0].innerHTML = "&larr;";
+      button2[1].innerHTML = "&#8682;";
+    } else {
+      button2[0].textContent = "Back Space";
+      button2[1].innerHTML = "Caps Lock";
+    }
+  },
+  false
+);
+//End of Change a character on specific breakpoint
+
+// Functions //
+
+function buttonClick(id) {
+  const uppText = id.toUpperCase();
+  const lowText = id.toLowerCase();
+
+  if (
+    capsLock.classList.contains("active") ||
+    shift.classList.contains("active")
+  ) {
+    textArea.innerHTML += uppText;
+    shift.classList.remove("active");
+  } else {
+    textArea.innerHTML += lowText;
+  }
+}
+
+function specialButton(id) {
+  text = textArea.textContent;
+
+  if (id === "backSpace") {
+    const str = textArea.value.substring(0, text.length - 1);
+    textArea.innerHTML = str;
+  }
+
+  if (id === "capsLock") {
+    capsLock.classList.toggle("active");
+  }
+
+  if (id === "shift") {
+    shift.classList.toggle("active");
+  }
+  if (id === "enter") {
+    textArea.innerHTML += "\n";
+  }
+
+  if (id === "submit") {
+    alert(textArea.value);
+  }
+  if (id === "space") {
+    textArea.innerHTML += "&nbsp;";
+  }
+}
